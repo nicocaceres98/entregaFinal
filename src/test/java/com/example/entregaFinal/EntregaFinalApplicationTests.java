be1a6d8d9ccad1bcc7dbd5c1e51c786f8041aca2
Nicolas Caceres <nicocaceres98.nc@gmail.com>
2023-03-14 12:40:51 -0300

    estructura y  modelado

---
+++ src/test/java/com/example/entregaFinal/EntregaFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.entregaFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregaFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
